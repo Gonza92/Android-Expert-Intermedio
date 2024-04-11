cd2c084f78ed6e8f8261af92e865d241a107dcab
gcota <gcotarelo.92@gmail.com>
2024-04-11 10:53:43 -0300

    Ya está la primer pantalla creada funcionando

--- app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -6,8 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.horoscapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
@@ -15,6 +22,42 @@ class HoroscopeFragment : Fragment() {
     private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        initList()
+        initUiState()
+    }
+
+    private fun initList() {
+        horoscopeAdapter = HoroscopeAdapter()
+
+        binding.rvHoroscope.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = horoscopeAdapter
+        }
+
+    }
+
+    private fun initUiState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    //Cambios en lista de horoscope
+                    horoscopeAdapter.updateList(it)
+
+                }
+            }
+        }
+
+    }
 
     override fun onCreateView(
 
